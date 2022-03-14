62d8110a731f6963bf0b9723f265158d621e66ad
FRC <frc@Mikes-MacBook-Pro-2.local>
2022-03-14 18:11:00 -0400

    Experimental speed limit button
    Mapped to depressing the left joystick.

---
+++ src/main/java/frc/robot/commands/ToggleSpeedLimit.java
@@ -0,0 +1,43 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.DifferentialDrivetrain;
+
+public class ToggleSpeedLimit extends CommandBase {
+  private final DifferentialDrivetrain m_drivetrain;
+  /** Creates a new ToggleSpeedLimit. */
+  public ToggleSpeedLimit(DifferentialDrivetrain drivetrain) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    m_drivetrain = drivetrain;
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    if (m_drivetrain.isSpeedLimited()){
+      m_drivetrain.disableSpeedLimit();
+    }
+    else {
+      m_drivetrain.enableSpeedLimit();
+    }
+        
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {}
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return true;
+  }
+}

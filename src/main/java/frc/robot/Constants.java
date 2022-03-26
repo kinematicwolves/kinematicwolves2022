7230687479b747d7e369f390603a4af9eb32baf8
Oive-OiL <alexmontesinos4321@gmail.com>
2022-03-25 22:43:12 -0400

    Re-organized Constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -13,46 +13,50 @@ package frc.robot;
  * constants are needed, to reduce verbosity.
  */
 public final class Constants {
-    // Ports on the driver station where the contollers are connected
+  // Ports on the driver station where the contollers are connected
     public static final int DRIVER_CONTROLLER = 0;
     public static final int MANIPULATOR_CONTROLLER = 1;
 
-    // Drivetrain subsystem controllers
+  //CAN ID's
     public static final int RIGHT_FRONT_DRIVE_MOTOR = 1;
     public static final int RIGHT_REAR_DRIVE_MOTOR = 2;
     public static final int LEFT_FRONT_DRIVE_MOTOR = 3;
     public static final int LEFT_REAR_DRIVE_MOTOR = 4;
+    public static final int VERTICALCONVEYORMOTOR = 7;
+    public static final int PNEUMATIC_CONTROL_MODULE = 9;
+    public static final int SHOOTER_MOTOR1 = 15;
+    public static final int HORIZONTALCONVEYORMOTOR = 17;  
+    public static final int INTAKE_MOTOR = 20;
+    public static final int CLIMBER_MOTOR1 = 24;
+    /*public static final int CLIMBER_MOTOR2 = 1000;*/
 
-    // Drivetrain PID parameters
-    public static final int DRIVETRAIN_MOTOR_PID_LOOP = 0;
-    public static final int DRIVETRAIN_MOTOR_PID_TIMEOUT = 0;
+  // Linear actuator
+    public static final int LINEAR_ACTUATOR_1 = 0; // Unverified
 
-    // Drivetrain rate limiters
-    // Slew rate limiters
+  // Tables for shooting speeds vs distance
+    public static final double[] SHOOTER_SPEEDS_RPM_ARRAY = {
+      4300, 4500, 5000, 5750
+     };
+          
+    public static final double[] TARGET_DISTANCE_INCHES_ARRAY = {
+     66, 91, 106, 132
+     };  
+
+  // Drivetrain slew rate limiters
     public static final double SLEW_RATE_LIMIT_ROTATE = 2.7;
-    public static final double SLEW_RATE_LIMIT_ACCEL = 3.9;
+    public static final double SLEW_RATE_LIMIT_ACCEL = 3.9;  
 
-    //motor speeds
+  //Drivetrain auton speeds
     public static final double AUTON_SPEED = 0.41;
 
-    // Intake Motor
-    public static final int INTAKE_MOTOR = 20;
-    public static final double DEFAULT_INTAKE_OUTPUT = -1;
-
-    // Pnuematic 
+  // Solenoid Mappings
+    public static final int DRVTRN_SOL_FWD_CHN = 0;
+    public static final int DRVTRN_SOL_RVS_CHN = 1;  
+  // Pnuematics
     public static final int INTAKE_SOLENOID_FWD = 2;
     public static final int INTAKE_SOLENOID_RVS = 3; 
 
-    public static final int PNEUMATIC_CONTROL_MODULE = 9;
-
-   // Solenoid Mappings
-    public static final int DRVTRN_SOL_FWD_CHN = 0;
-    public static final int DRVTRN_SOL_RVS_CHN = 1;
-
-    //Shooter Motor can id's   
-    public static final int SHOOTER_MOTOR1 = 15;    
-
-    // Shooter motor PID constants for velocity control
+  // Shooter motor PID constants for velocity control
     public static final double SHOOTER_Kf = 0.051;
     public static final double SHOOTER_Kp = 0.05;
     public static final double SHOOTER_Ki = 0;
@@ -60,50 +64,38 @@ public final class Constants {
     public static final int SHOOTER_PID_SLOT = 0;
     public static final int kTimeoutMs = 30; // Used for all PID loops
 
-    // Tables for shooting speeds vs distance
-    public static final double[] SHOOTER_SPEEDS_RPM_ARRAY = {
-      4500, 4800, 5000, 5200, 6000
-    };
-      
-    public static final double[] TARGET_DISTANCE_INCHES_ARRAY = {
-      68, 85, 98, 112, 145
-    };  
-    
-    //Climber Motors
-    public static final int CLIMBER_MOTOR1 = 24;
-    public static final int CLIMBER_MOTOR2 = 1000;
-    public static final double DEFAULT_CLIMBER_OUTPUT = 0.75; 
-    
-    // Linear actuator
-    public static final int LINEAR_ACTUATOR_1 = 0; // Unverified
-
-    // Climber PID constants for position control
+  // Climber 1 PID constants for position control
     public static final double CLIMBER1_Kf = 0.051;
     public static final double CLIMBER1_Kp = 0.05;
     public static final double CLIMBER1_Ki = 0;
     public static final double CLIMBER1_Kd = 0.5;
     public static final int CLIMBER1_PID_SLOT = 0;
 
+  // Climber 2 PID constants for position control
     public static final double CLIMBER2_Kf = 0.051;
     public static final double CLIMBER2_Kp = 0.05;
     public static final double CLIMBER2_Ki = 0;
     public static final double CLIMBER2_Kd = 0.5;
     public static final int CLIMBER2_PID_SLOT = 0;
 
+  //Climber pnuematic Brake
     public static final int CLIMBER_BRAKE_FWD_CHN = 4;
     public static final int CLIMBER_BRAKE_RVS_CHN = 4;
 
-    //Conveyor Motor can id's
-    public static final int HORIZONTALCONVEYORMOTOR = 17;
-    public static final int VERTICALCONVEYORMOTOR = 7;
-    public static final double DEFAULT_HORIZONTAL_CONVEYOR_OUTPUT = 1;
-    public static final double DEFAULT_VERTICAL_CONVEYOR_OUTPUT = 1;
-
-    // Vision Values
+  // Vision Values
     public static final float LIMELIGHT_VERTICAL_ANGLE = (float) 44; //change
     public static final float LIMELIGHT_HEIGHT = (float) 21; //change, inches         
     public static final float TARGET_HEIGHT = (float) 104; //change, inches            
     // public static final double RotationRate = 1.0; //change
   
-    
+  // Drivetrain PID parameters
+    public static final int DRIVETRAIN_MOTOR_PID_LOOP = 0;
+    public static final int DRIVETRAIN_MOTOR_PID_TIMEOUT = 0;
+
+  // Default Motor Speeds
+  public static final double DEFAULT_INTAKE_OUTPUT = -1;
+  public static final double DEFAULT_CLIMBER_OUTPUT = 0.75; 
+  public static final double DEFAULT_HORIZONTAL_CONVEYOR_OUTPUT = 1;
+  public static final double DEFAULT_VERTICAL_CONVEYOR_OUTPUT = 1;
+  
 }
--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,12 @@
 
 package frc.robot;
 
-import javax.imageio.plugins.jpeg.JPEGHuffmanTable;
-
-import org.ejml.ops.ConvertMatrixData;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.commands.AlignWithTarget;
-import frc.robot.commands.BackupShootAuton;
-//import frc.robot.commands.BackupShootAuton;
 import frc.robot.commands.BackwordsAuton;
 import frc.robot.commands.DeployIntake;
 import frc.robot.commands.DriveRobotOpenLoop;
@@ -22,7 +17,6 @@ import frc.robot.commands.RunClimber1OpenLoop;
 import frc.robot.commands.RunHorizontalConveyor;
 import frc.robot.commands.RunIntakeMotor;
 import frc.robot.commands.RunVerticalConveyor;
-import frc.robot.commands.SetShooterToSpeed;
 import frc.robot.commands.ShiftGear;
 import frc.robot.commands.ShootWithLTrigger;
 import frc.robot.commands.ToggleSpeedLimit;
@@ -78,6 +72,7 @@ public class RobotContainer {
     JoystickButton dc_aButton = new JoystickButton(m_driverController, XboxController.Button.kA.value);
     JoystickButton dc_bButton = new JoystickButton(m_driverController, XboxController.Button.kB.value);
     JoystickButton dc_leftStickButton = new JoystickButton(m_driverController, XboxController.Button.kLeftStick.value);
+    JoystickButton dc_rButton = new JoystickButton(m_driverController, XboxController.Button.kRightBumper.value);
 
     JoystickButton dc_yButton = new JoystickButton(m_driverController, XboxController.Button.kY.value);
     JoystickButton mc_aButton = new JoystickButton(m_manipulatorController, XboxController.Button.kA.value);
@@ -91,21 +86,23 @@ public class RobotContainer {
 
     //Driver Controller
     dc_aButton.whenPressed(new ShiftGear(m_pneumaticSubsystem, m_drivetrainSubsystem));
-    //dc_bButton.whileHeld(new RunClimber1OpenLoop(m_climberSubsystem, 0.2));
     dc_leftStickButton.whenPressed(new ToggleSpeedLimit(m_drivetrainSubsystem));
     dc_yButton.whileHeld(new RunClimber1OpenLoop(m_climberSubsystem, -0.2));
-    dc_bButton.whileHeld(new RunClimber1OpenLoop(m_climberSubsystem, -1));
+    dc_bButton.whileHeld(new RunClimber1OpenLoop(m_climberSubsystem, -0.55));
+    dc_rButton.whileHeld(new AlignWithTarget(m_visionSubsystem, m_drivetrainSubsystem, 0.31));
   
     //Munipulator Controller 
-    mc_yButton.whileHeld(new RunHorizontalConveyor(m_hConveyorSubsystem, Constants.DEFAULT_HORIZONTAL_CONVEYOR_OUTPUT)); //reversed
-    mc_aButton.whileHeld(new RunIntakeMotor(m_intakeSubsystem, Constants.DEFAULT_INTAKE_OUTPUT));
-    mc_aButton.whileHeld(new RunHorizontalConveyor(m_hConveyorSubsystem, -1* Constants.DEFAULT_HORIZONTAL_CONVEYOR_OUTPUT));
-    mc_yButton.whileHeld(new RunIntakeMotor(m_intakeSubsystem, -1 * Constants.DEFAULT_INTAKE_OUTPUT)); //reversed
-    mc_bButton.whileHeld(new RunIntakeMotor(m_intakeSubsystem, -1 * Constants.DEFAULT_INTAKE_OUTPUT)); //reversed 
-    mc_rButton.whileHeld(new RunVerticalConveyor(m_vConveyorSubsystem, Constants.DEFAULT_VERTICAL_CONVEYOR_OUTPUT));
+    //-RunIntakeMotor = Horizontal Conveyor
+    //-RunHorizontalConveyor = Intake Motor
+    mc_aButton.whileHeld(new RunIntakeMotor(m_intakeSubsystem, 1));
+    mc_aButton.whileHeld(new RunHorizontalConveyor(m_hConveyorSubsystem, -1));
+    mc_aButton.whileHeld(new RunVerticalConveyor(m_vConveyorSubsystem, 0.1));
+    mc_yButton.whileHeld(new RunIntakeMotor(m_intakeSubsystem, -1)); //reversed
+    mc_yButton.whileHeld(new RunHorizontalConveyor(m_hConveyorSubsystem, 1)); //reversed
+    mc_bButton.whileHeld(new RunIntakeMotor(m_intakeSubsystem, -1)); //reversed 
     mc_xButton.whenPressed(new DeployIntake(m_pneumaticSubsystem, m_intakeSubsystem));
-    mc_rButton.whenHeld(new RunIntakeMotor(m_intakeSubsystem, Constants.DEFAULT_INTAKE_OUTPUT));
-    mc_lButton.whileHeld(new AlignWithTarget(m_visionSubsystem, m_drivetrainSubsystem, 0.31));
+    mc_rButton.whileHeld(new RunVerticalConveyor(m_vConveyorSubsystem, 0.8));
+    mc_lButton.whileHeld(new RunIntakeMotor(m_intakeSubsystem, 1));
   }
 
   

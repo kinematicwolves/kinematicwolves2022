c1aee4bcf6bf7fe2452709d5f4d169eda5ae3066
Ann McGowan <kinematicwolves@gmail.com>
2022-04-11 18:24:59 -0400

    led count changed

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -103,7 +103,7 @@ public final class Constants {
 
   public static class CandleConstants {
     public static final int CANDLE_1_ID = 50;
-    public static final int CANDLE_1_LED_COUNT = 308;
+    public static final int CANDLE_1_LED_COUNT = 185;
     
     //public static final int CANDLE_2_ID = 10001;
     //public static final int CANDLE_2_LED_COUNT = 11;
